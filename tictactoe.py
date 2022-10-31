ceee668aac47522df55fea35152aea503c11099d
stoogero <robicodes7@gmail.com>
2022-10-31 08:34:46 -0400

    add initial setup

---
+++ tictactoe.py
@@ -0,0 +1,28 @@
+import random
+
+def drawBoard(board):
+    print('   |   |')
+    print('' + board[7] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+def inputPlayerLetter():
+    letter = ''
+    while not (letter == 'X' or letter == 'O'):
+        print('Do you want to be X or O?')
+        letter = input().upper()
+    
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
+
+def whoGoesFirst():
+    
